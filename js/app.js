3ae709fc53c702ac6e8015e844c1731dc731cd76
Daniele T <Fupete@users.noreply.github.com>
2024-02-04 23:17:58 +0100

    feat: add double tab random sketch

--- js/app.js
+++ js/app.js
@@ -398,3 +398,28 @@ const playDirector = () => {
 		playDirector()
 	}, randSec * 1000);
 }
+
+/* Based on this http://jsfiddle.net/brettwp/J4djY/*/
+const detectDoubleTapClosure = () => {
+  let lastTap = 0;
+  let timeout;
+  return function detectDoubleTap(event) {
+    const curTime = new Date().getTime();
+    const tapLen = curTime - lastTap;
+    if (tapLen < 500 && tapLen > 0) {
+      // console.log('Double tapped!');
+      randomSketch();
+      event.preventDefault();
+    } else {
+      timeout = setTimeout(() => {
+        clearTimeout(timeout);
+      }, 500);
+    }
+    lastTap = curTime;
+  };
+}
+
+/* Regex test to determine if user is on mobile */
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    document.body.addEventListener('touchend', detectDoubleTapClosure(), { passive: false });
+}
